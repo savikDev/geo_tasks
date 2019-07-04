a7589ac044117c4f8fd7f316eba9087c69c3ab50
Andrii Savenkov <andrii.savenkov@apartmenttherapy.com>
2019-07-04 10:57:53 +0300

    add seed data

--- apps/geo_tasks_db/priv/repo/seeds.exs
+++ apps/geo_tasks_db/priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+GeoTasksDb.create_new_role(%{name: "Manager"})
+GeoTasksDb.create_new_role(%{name: "Driver"})
+
+{:ok, m1} = GeoTasksDb.create_new_token(%{role_name: "Manager"})
+{:ok, m2} = GeoTasksDb.create_new_token(%{role_name: "Manager"})
+GeoTasksDb.create_new_token(%{role_name: "Manager"})
+
+{:ok, d1} = GeoTasksDb.create_new_token(%{role_name: "Driver"})
+{:ok, d2} = GeoTasksDb.create_new_token(%{role_name: "Driver"})
+{:ok, d3} = GeoTasksDb.create_new_token(%{role_name: "Driver"})
+GeoTasksDb.create_new_token(%{role_name: "Driver"})
+GeoTasksDb.create_new_token(%{role_name: "Driver"})
+
+GeoTasksDb.activate_token(d1.id)
+GeoTasksDb.activate_token(d2.id)
+GeoTasksDb.activate_token(d3.id)
+
+GeoTasksDb.activate_token(m1.id)
+GeoTasksDb.activate_token(m2.id)
