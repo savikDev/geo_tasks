b4a8a8a169a8cb408a8240ef75dcbddbd1544a6a
Andrii Savenkov <andrii.savenkov@apartmenttherapy.com>
2019-07-04 09:12:11 +0300

    add postgis

--- apps/geo_tasks_db/priv/repo/migrations/20190622073532_delivery_locations.exs
+++ apps/geo_tasks_db/priv/repo/migrations/20190622073532_delivery_locations.exs
@@ -2,6 +2,7 @@ defmodule GeoTasksDb.Repo.Migrations.DeliveryLocations do
   use Ecto.Migration
 
   def up do
+    execute "CREATE EXTENSION IF NOT EXISTS postgis"
     create table(:delivery_locations, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :location, :geometry
@@ -11,6 +12,7 @@ defmodule GeoTasksDb.Repo.Migrations.DeliveryLocations do
   end
 
   def down do
+    execute "DROP EXTENSION IF EXISTS postgis"
     drop table(:delivery_locations)
   end
 end
--- apps/geo_tasks_db/priv/repo/migrations/20190622073544_pickup_locations.exs
+++ apps/geo_tasks_db/priv/repo/migrations/20190622073544_pickup_locations.exs
@@ -2,6 +2,7 @@ defmodule GeoTasksDb.Repo.Migrations.PickupLocations do
   use Ecto.Migration
 
   def up do
+    execute "CREATE EXTENSION IF NOT EXISTS postgis"
     create table(:pickup_locations,  primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :location, :geometry
@@ -11,6 +12,7 @@ defmodule GeoTasksDb.Repo.Migrations.PickupLocations do
   end
 
   def down do
+    execute "DROP EXTENSION IF EXISTS postgis"
     drop table(:pickup_locations)
   end
 end
